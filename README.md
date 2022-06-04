3154beb825fee48d05ef6fd31237335e1eab52e7
Revaldo Putra <revaldoputra11@gmail.com>
2022-06-04 16:15:51 +0700

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# ✔️ Flutter to do App "To Do List"
+
+A Flutter app based on the design of the To Do App, created by *Rudi Hartono*, see more on: [Uplabs](https://www.uplabs.com/posts/to-do-list-app-freebie-kit).
+
+## Getting Started 🚀
+
+```shell
+- Clone the repo
+- Install the dependicies
+- Run it
+```
+
+## Preview
+
+The app is based on Rudi's design:
+
+The result is:
+
+![App preview](doc/AppPreview.gif)
+
---
+++ lib/home.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/appBars.dart';
+import 'package:todo_app/bottomNavigation.dart';
+import 'package:todo_app/fab.dart';
+import 'package:todo_app/util.dart';
+
+class Home extends StatefulWidget {
+  // Home({required Key key}) : super(key: key);
+
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final bottomNavigationBarIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: fullAppbar(context),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 15, left: 20, bottom: 15),
+              child: Text(
+                'Today',
+                style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w600,
+                    color: CustomColors.TextSubHeader),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 15),
+              padding: EdgeInsets.fromLTRB(5, 13, 5, 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Image.asset('assets/images/checked.png'),
+                  Text(
+                    '07.00 AM',
+                    style: TextStyle(color: CustomColors.TextGrey),
+                  ),
+                  Container(
+                    width: 180,
+                    child: Text(
+                      'Go jogging with Christin',
+                      style: TextStyle(
+                          color: CustomColors.TextGrey,
+                          //fontWeight: FontWeight.w600,
+                          decoration: TextDecoration.lineThrough),
+                    ),
+                  ),
+                  Image.asset('assets/images/bell-small.png'),
+                ],
+              ),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  stops: [0.015, 0.015],
+                  colors: [CustomColors.YellowIcon, Colors.white],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: CustomColors.GreyBorder,
+                    blurRadius: 10.0,
+                    spreadRadius: 5.0,
+                    offset: Offset(0.0, 0.0),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15)
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: customFab(context),
+      bottomNavigationBar:
+          BottomNavigationBarApp(context, bottomNavigationBarIndex),
+    );
+  }
+}
